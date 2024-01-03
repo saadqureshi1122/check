7b08b00e17a2304129c3624e198cb2d616163107
saadqureshi1122 <saadqurashi333@gmail.com>
2024-01-03 21:07:59 +0500

    changes

--- src/db/index.js
+++ src/db/index.js
@@ -3,8 +3,10 @@ import { DB_NAME } from "../constants.js";
 
 const connectionDB = async () => {
   try {
-    mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
-    console.log("Connected to Mongo DB");
+    const connectionInstance = await mongoose.connect(
+      `${process.env.MONGODB_URI}/${DB_NAME}`
+    );
+    console.log("Connected to Mongo DB" + connectionInstance.connection.host);
   } catch (error) {
     console.log("Connection Failed", error);
   }
